02910bd81bc83f781913d6309cce12334df8a777
p srinivas <100501528+srinivaspagoti@users.noreply.github.com>
2022-03-16 20:29:28 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,24 @@
+# a={}
+# print(a,type(a))
+# g=(1,5,9,7)
+# print(g,type(g))
+# i=frozenset(g)
+# print(i)
+# s={5,9,7,2}
+# s.add(4)
+# print(s)
+# a={1,2,3,4,5}
+# b="naidu",5,8,9
+# a.add(b)
+# print(a)
+# t={7,9,5,4,3}
+# t.clear()
+# print(t)
+# a={20,30,40,50,};b={10,15,25,35,45};c={20,20,30,40}
+# print(a.intersection(b))
+# print(b.intersection(c))
+# print(c.intersection(a))
+a={3,4,5,6}
+b={6,7,8,9}
+print(a.union(b))
+print(b.union(a))
